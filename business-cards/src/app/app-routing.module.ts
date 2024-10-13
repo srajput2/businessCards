a86d3c2460a66629a614da6949dd2d078934016f
srajput9232 <sourabh.subhash.rajput@gmail.com>
2024-10-12 20:12:30 -0400

    Created login page with Styling
    Created login page with Styling

--- business-cards/src/app/app-routing.module.ts
+++ business-cards/src/app/app-routing.module.ts
@@ -8,9 +8,18 @@ const routes: Routes = [
   },
   {
     path: '',
-    redirectTo: 'home',
+    redirectTo: 'loader',
     pathMatch: 'full'
   },
+  {
+    path: 'loader',
+    loadChildren: () => import('./pages/loader/loader.module').then( m => m.LoaderPageModule)
+  },  {
+    path: 'login',
+    loadChildren: () => import('./pages/login/login.module').then( m => m.LoginPageModule)
+  },
+
+
 ];
 
 @NgModule({
